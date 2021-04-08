b3fa9b3059ffd43fc473ca780f258b777a5748e0
Andrey B <bazh.andrey8@gmail.com>
2021-04-08 19:04:43 +0300

    add framer-motion

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import {MainContainer,MainBg,VideoBg,MainBtnWrapper,MainContent,MainP,MainH1,ArrowForward,ArrowRight} from "./MSElements";
 import Video from '../../videos/video.mp4'
 import {Button} from "../ButtonElement";
-
+import {motion} from 'framer-motion'
 const MainSection = () => {
 const[hover,setHover]=useState(false)
 
@@ -17,13 +17,51 @@ const[hover,setHover]=useState(false)
             <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
             </MainBg>
             <MainContent>
+                <motion.div
+                initial={{
+                    opacity:0
+                }}
+                animate={{
+                    opacity:1
+                }}
+                transition={{
+                    delay:1,
+                    duration:2
+                }}
+                >
                 <MainH1>
                     Перепончатокрылатые насекомые
                 </MainH1>
+               </motion.div>
+                <motion.div
+                    initial={{
+                        opacity:0
+                    }}
+                    animate={{
+                        opacity:1
+                    }}
+                    transition={{
+                        delay:2,
+                        duration:2
+                    }}
+                >
                 <MainP>
                      Блог о видах,особенностях , насекомых отряда перепончатокрылатых.Зарегистрируйся, чтобы узнать больше.
                 </MainP>
+                </motion.div>
                 <MainBtnWrapper>
+                    <motion.div
+                        initial={{
+                            opacity:0
+                        }}
+                        animate={{
+                            opacity:1
+                        }}
+                        transition={{
+                            delay:3,
+                            duration:2
+                        }}
+                    >
                     <Button to='signup'
                             onMouseEnter={onHover}
                             onMouseLeave={onHover}
@@ -33,6 +71,7 @@ const[hover,setHover]=useState(false)
                         Погнали{ hover?<ArrowForward/>:<ArrowRight/>}
 
                     </Button>
+                    </motion.div>
                 </MainBtnWrapper>
             </MainContent>
         </MainContainer>
